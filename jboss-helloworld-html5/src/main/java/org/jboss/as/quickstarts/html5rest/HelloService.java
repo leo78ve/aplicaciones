52da87de8f246f3766920d78a1bba1afbd935c49
Leonardo Rodriguez <lleon@redhat.com>
2017-04-24 20:14:35 -0500

    Cambio de mensaje

--- jboss-helloworld-html5/src/main/java/org/jboss/as/quickstarts/html5rest/HelloService.java
+++ jboss-helloworld-html5/src/main/java/org/jboss/as/quickstarts/html5rest/HelloService.java
@@ -25,7 +25,7 @@ package org.jboss.as.quickstarts.html5rest;
 public class HelloService {
 
     String createHelloMessage(String name) {
-        return "Hola otra vez " + name + "!";
+        return "Hola " + name + "!" + " demo para GNP";
     }
 
 }
